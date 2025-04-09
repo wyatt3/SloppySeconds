c6f4697f650f03611be6a93154d3e7a4770b1219
Wyatt Johnson <wyatt@sliger.com>
2025-04-09 15:11:03 -0600

    add recipe controller and service

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recipe;
+
+class RecipeController extends Controller
+{
+    public function index()
+    {
+        return;
+    }
+
+    public function show(Recipe $recipe)
+    {
+        return;
+    }
+}

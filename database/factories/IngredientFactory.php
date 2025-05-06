339cd600010788df0d34904dc19aec41a4f4672d
Wyatt Johnson <wyatt@sliger.com>
2025-05-06 09:04:07 -0600

    add ingredient factory

---
+++ database/factories/IngredientFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Recipe;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ingredient>
+ */
+class IngredientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->words(3, true),
+            'recipe_id' => Recipe::factory(),
+            'amount' => $this->faker->numberBetween(1, 10),
+            'unit' => $this->faker->word(),
+        ];
+    }
+}

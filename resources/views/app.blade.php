09c2449a907d9f1a93246d5d1d0c8eb7bb6f7a79
Wyatt Johnson <wyatt.j1834@gmail.com>
2025-07-19 13:44:58 -0600

    set up tailwind properly

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @vite(['resources/css/app.css', 'resources/sass/app.scs', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
 
